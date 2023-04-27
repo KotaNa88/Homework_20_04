5fa1954d3a891aa96f25c296ed33dd885ee13775
Timur Gareev <gti8833@gmail.com>
2023-04-27 15:43:17 +0300

    Homework

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//Task1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число больше, а какое меньше.
+
+// Console.WriteLine("Input num1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input num2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2)
+// {
+//     Console.WriteLine($"{num1} больше {num2}");
+// }
+// else
+// Console.WriteLine($"{num2} больше {num1}");
+
+//Task2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Input num1");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input num2");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input num3");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+
+// if (num2 > num1) max = num2;
+// if (num3 > num2) max = num3;
+
+// Console.WriteLine($"{max} maximum number");
+
+//Task3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 2 == 0)
+// {
+//     Console.WriteLine($"{number} number is even");
+// }
+  
+// else
+// Console.WriteLine($"{number} not an even number");
+
+
+
+//Task4 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Input N");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// for (int count = 1; count <= N; count++)
+//     if (count % 2 == 0)
+//         Console.WriteLine(count);
